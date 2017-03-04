e5dff30cfa48a274ac3ce9700c5951224660cee8
Domingos Lopes <domingos86lopes+github@gmail.com>
2017-03-04 15:27:08 -0500

    include manager_id - untested

--- renthop_preprocessing/__init__.py
+++ renthop_preprocessing/__init__.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 
+from sklearn.utils import shuffle as skshuffle
+
+def shuffle(X, y):
+	if isinstance(X, dict):
+        to_shuffle = X.keys()
+        to_shuffle.append(y)
+        shuffled = skshuffle(*to_shuffle)
+        return dict(zip(X.keys(), shuffled[:-1])), shuffled[-1]
+    else:
+        return skshuffle(X, y)
--- renthop_preprocessing/preset_preprocessors.py
+++ renthop_preprocessing/preset_preprocessors.py
@@ -21,3 +21,26 @@ def features_sentiment_preprocessor():
     preprocessor.with_pipeline('response').set_loader(json_loader.select_loader('interest_level'))
     preprocessor.add_operation(loaders.Dummifier(output_cols = ['high', 'medium', 'low'])).add_operation(loaders.ToNdarray())
     return preprocessor
+
+def features_sentiment_manager_preprocessor():
+    json_loader = loaders.JSONLoader()
+    preprocessor = loaders.Preprocessor()
+    preprocessor.with_pipeline('main').set_loader(json_loader)
+    preprocessor.add_operation(loaders.DateTimeExtractor()).add_operation(loaders.NewSimplePredictors())
+    preprocessor.add_operation(loaders.LogTransform(['price_per_bedroom', 'price', 'price_per_bathroom']))
+    preprocessor.add_operation(loaders.Selector(['listing_id', 'bathrooms', u'bedrooms', 'latitude', 'longitude', 'price',
+                                                 'month', 'day_of_month', 'hour', 'day_of_week', 'price_per_bathroom',
+                                                 'price_per_bedroom', 'num_features', 'features_len', 'num_photos']))
+    merger = loaders.PandasColumnMerger(['main', 'features', 'sentiment'], on = 'listing_id')
+    preprocessor.set_consumer(merger)
+    preprocessor.with_pipeline('features').set_loader(loaders.CSVLoader('data/features_train.csv', 'data/features_test.csv'))
+    preprocessor.set_consumer(merger)
+    preprocessor.with_pipeline('sentiment').set_loader(loaders.CSVLoader('data/sentiment_train.csv', 'data/sentiment_test.csv'))
+    preprocessor.set_consumer(merger)
+    preprocessor.with_pipeline('merged').set_loader(merger).add_operation(loaders.ColumnDrop('listing_id'))
+    preprocessor.add_operation(loaders.ToNdarray()).add_operation(preprocessing.StandardScaler())
+    preprocessor.with_pipeline('response').set_loader(json_loader.select_loader('interest_level'))
+    preprocessor.add_operation(loaders.Dummifier(output_cols = ['high', 'medium', 'low'])).add_operation(loaders.ToNdarray())
+    preprocessor.with_pipeline('managers').set_loader(json_loader.select_loader('manager_id'))
+    preprocessor.add_operation(loaders.CategoricalFilter(1000))
+    return preprocessor
